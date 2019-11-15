d27c988d361cd60d1c43f07e68f4c99071757f35
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-15 16:26:03 +0000

    color database created

--- lib/tempestry_pattern.rb
+++ lib/tempestry_pattern.rb
@@ -4,13 +4,13 @@ require 'pry'
 require 'date'
 require 'sqlite3'
 
-require "tempestry_pattern/version"
+require_relative './tempestry_pattern/version'
 require_relative './tempestry_pattern/color'
 require_relative './tempestry_pattern/scraper'
 require_relative './tempestry_pattern/weather'
 require_relative './tempestry_pattern/cli'
 
-module TempestryPattern
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module TempestryPattern
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
